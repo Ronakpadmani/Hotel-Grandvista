c0997103365192af30792b70a6719234dbd4e8dd
Ronakpadmani <157878368+Ronakpadmani@users.noreply.github.com>
2025-07-11 10:12:53 +0530

    Update HotelBookingApplication.java

--- HotelBooking-backend/HotelBooking/src/main/java/com/example/HotelBooking/HotelBookingApplication.java
+++ HotelBooking-backend/HotelBooking/src/main/java/com/example/HotelBooking/HotelBookingApplication.java
@@ -8,24 +8,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class HotelBookingApplication {
 
-//	@Autowired
-//	private NotificationService notificationService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelBookingApplication.class, args);
 	}
 
-//	@Override
-//	public void run(String... args) throws Exception {
-//
-//		NotificationDTO notificationDTO = NotificationDTO.builder()
-//				.type(NotificationType.EMAIL)
-//				.recipient("ronakpadmani2903@gmail.com")
-//				.body("I am testing from a command line")
-//				.subject("Testing Email Sending")
-//				.build();
-//
-//		notificationService.sendEmail(notificationDTO);
-//
-//	}
+
 }
